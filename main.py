6a4382cc603cbb2f2c1ff4586d2838725b79e29b
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-03-27 23:31:09 +0100

    Add Edit and Delete Endpoints

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, model_validator
 from database import SessionLocal, init_db
 from typing import List
 from models import Job
 
+
 app = FastAPI()
-jobs = []
 
 @app.on_event("startup")
 def on_startup():
@@ -18,14 +18,16 @@ def get_db():
         yield db
     finally:
         db.close()
+
 class JobRequest(BaseModel):
     id: int
-    title: str
-    company: str
-    status: str
+    title: str = Field(min_length=3, max_length=50)
+    company: str = Field(min_length=2, max_length=50)
+    status: str  # NEEDS VALIDATION!
 
     class Config:
         orm_mode = True
+
 @app.get("/")
 def home():
     return {'message': 'Job Tracker API is running..'}
@@ -53,3 +55,30 @@ def post_jobs(job: JobRequest, db: Session = Depends(get_db)):
         "status": db_job.status,
     }
 
+# Edits entry
+@app.put("/jobs/{id}", response_model=JobRequest)
+def edit_job(id: int, job: JobRequest, db: Session = Depends(get_db)):
+    db_job = db.query(Job).filter(Job.id == id).first()
+
+    if not db_job:
+        raise HTTPException(status_code = 404, detail = "Job not found.")
+    
+    db_job.title = job.title
+    db_job.company = job.company
+    db_job.status = job.status
+
+    db.commit()
+    db.refresh(db_job)
+    return db_job
+
+# Deletes entry
+@app.delete("/jobs/{id}", status_code=204)
+def delete_job(id: int, job: JobRequest, db: Session = Depends(get_db)):
+    db_job = db.query(Job).filter(Job.id == id).first()
+
+    if not db_job:
+        raise HTTPException(status_code = 404, detail = "Job not found.")
+    
+    db.delete(db_job)
+    db.commit()
+    return None
