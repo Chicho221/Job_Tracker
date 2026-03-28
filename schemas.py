fbba1495b58dea6f356bc1bd33733bd9e896b6af
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-03-28 01:19:23 +0100

    Add User Schemas

--- schemas.py
+++ schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
 
+# Job Schemas
 class JobBase(BaseModel):
     title: str = Field(min_length=3, max_length=50)
     company: str = Field(min_length=2, max_length=50)
@@ -11,4 +12,17 @@ class JobCreate(JobBase):
 class Job(JobBase):
     id: int
     class Config:
-        orm_model = True
+        orm_mode = True
+
+# User Schemas
+class UserBase(BaseModel):
+    username : str
+
+class UserCreate(UserBase):
+    pass
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
