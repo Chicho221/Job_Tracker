e98ddee26cd3873f22820dccdbb4bada1ad90940
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-03-28 01:19:44 +0100

    Add User Endpoints

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from pydantic import BaseModel, Field, model_validator
 from database import SessionLocal, init_db
 from typing import List
-from models import JobModel
-from schemas import JobCreate, Job
+from passlib.context import CryptContext
+from models import JobModel, UserModel
+from schemas import JobCreate, Job, UserCreate, User
 
 
 app = FastAPI()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @app.on_event("startup")
 def on_startup():
@@ -23,6 +24,8 @@ def get_db():
 @app.get("/")
 def home():
     return {'message': 'Job Tracker API is running..'}
+
+## Job Endpoints
 
 # Pulls all enties from database
 @app.get("/jobs", response_model=List[Job])
@@ -72,5 +75,40 @@ def delete_job(id: int, job: JobCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code = 404, detail = "Job not found.")
     
     db.delete(db_job)
+    db.commit()
+    return None
+
+## User Endpoints
+
+# Show Users
+@app.get("/users", response_model=List[User])
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(UserModel).all()
+    return users
+
+# Create User
+@app.post("/users")
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    hashed = pwd_context.hash(user.password)
+    db_user = UserModel(username = user.username, password_hash = hashed)
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return {
+        "id": db_user.id,
+        "username": db_user.username,
+    }
+
+# Deletes User
+@app.delete("/users/{id}", status_code=204)
+def delete_user(id: int, user: UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(UserModel).filter(UserModel.id == id).first()
+
+    if not db_user:
+        raise HTTPException(status_code = 404, detail = "User not found.")
+    
+    db.delete(db_user)
     db.commit()
     return None
