d51add892658c67269cee757cdeaa4ead2e313d3
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-03-29 16:17:06 +0200

    Add pagination

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ def get_current_user(token : str = Depends(oauth2_scheme), db: Session = Depends
 
 # Display Jobs
 @app.get("/jobs", response_model=List[Job])
-def get_jobs(current_user: UserModel = Depends(get_current_user), db: Session = Depends(get_db)):
-    jobs = db.query(JobModel).filter(JobModel.user_id == current_user.id).all()
+def get_jobs(skip: int = 0, limit: int = 10, current_user: UserModel = Depends(get_current_user), db: Session = Depends(get_db)):
+    jobs = db.query(JobModel).filter(JobModel.user_id == current_user.id).offset(skip).limit(limit).all()
     return jobs
 
 # Create Job
