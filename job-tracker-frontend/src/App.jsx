1bdfe7be490a29cd4782946eef1ba0b4ed052ebf
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-04-01 00:47:08 +0200

    Add Pagination

--- job-tracker-frontend/src/App.jsx
+++ job-tracker-frontend/src/App.jsx
@@ -12,16 +12,26 @@ function App() {
   const [editingId, setEditingId] = useState(null)
   const [search, setSearch] = useState("")
   const [searchstatus, setSearchStatus] = useState("")
+  const [total, setTotal] = useState(0)
+  const [limit] = useState(5)
+  const [page, setPage] = useState(1)
 
   useEffect(() => {
     const savedToken = localStorage.getItem("token")
     if (savedToken) {
       setToken(savedToken)
     }
+  },[])
+  
+  useEffect(() => {
     if (token) {
       fetchJobs()
     }
-  },[search,searchstatus])
+  }, [token, page, search, searchstatus])
+
+  useEffect(() => {
+    setPage(1)
+  }, [searchstatus, search, token])
 
   //Login function
   const login = async () => {
@@ -33,23 +43,24 @@ function App() {
       method: "POST",
       body: formData
     })
-
+    if (response.ok) {
     const data = await response.json()
-
     setToken(data.access_token)
     localStorage.setItem("token", data.access_token)
+    }
   }
 
   //Get all/search jobs function
   const fetchJobs = async () => {
-    const response = await fetch(`http://127.0.0.1:8000/jobs?search=${search}&status=${searchstatus}`, {
+    const response = await fetch(`http://127.0.0.1:8000/jobs?search=${search}&status=${searchstatus}&skip=${(page-1)*limit}&limit=${limit}`, {
       headers: {
         Authorization: `Bearer ${token}`
       }
     })
     if (response.ok) {
       const data = await response.json()
-      setJobs(data)
+      setJobs(data.jobs)
+      setTotal(data.total)
     }
   }
 
@@ -114,7 +125,7 @@ return (
   <input placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
   <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
   
-  <button onClick={login}>Log In</button>
+  <button onClick={() => {login();fetchJobs()}}>Log In</button>
   <h1>Job Tracker</h1>
     <button onClick={fetchJobs}>Load Jobs</button>
 
@@ -132,14 +143,18 @@ return (
 
     <h2>Search</h2>
       <input placeholder="Search by: Title, Company" value={search} onChange={(e) => setSearch(e.target.value)}/>
-      <button onClick={() => {fetchJobs();setSearch("");}}>Search</button>
+      <button onClick={fetchJobs}>Search</button>
       <select value = {searchstatus} onChange = {(e) => setSearchStatus(e.target.value)}>
         <option value="" default>All</option>
         <option value="applied">Applied</option>
         <option value="rejected">Rejected</option>
         <option value="interview">Interview</option>
       </select>
-      
+      <div>
+        <button onClick={() => setPage(prev => Math.max(prev-1, 1))} disabled={page === 1}>Prev</button>
+        <span>Page {page} of {Math.ceil(total / limit)}</span>
+        <button onClick={() => setPage(prev => Math.min(prev+1, Math.ceil(total / limit)))} disabled={page === Math.ceil(total / limit)}>Next</button>
+      </div>
       <ul>
         {jobs.map((job) => (
           <li key = {job.id}>
@@ -153,6 +168,8 @@ return (
           </li>
         ))}
       </ul>
+
+
 
 
 </div>
