4d097ffef5fbf730055d75d088285938d482479b
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-04-01 06:26:29 +0200

    -Edit frontend -Add sidemenu -Add login/logout layout

--- job-tracker-frontend/src/App.jsx
+++ job-tracker-frontend/src/App.jsx
@@ -38,11 +38,12 @@ function App() {
     const formData = new URLSearchParams()
     formData.append("username", username)
     formData.append("password", password)
-
+    console.log("Debug: End")
     const response = await fetch("http://127.0.0.1:8000/login",{
       method: "POST",
       body: formData
     })
+    console.log("Debug: End")
     if (response.ok) {
     const data = await response.json()
     setToken(data.access_token)
@@ -119,59 +120,91 @@ function App() {
   }
 return (
 
-<div>
-  <h2>Login</h2>
+<div className="overflow-hidden w-full max-h-screen bg-black/10 text-white flex flex-row">
+  <div className="w-1/6 h-screen min-w-60 pt-6 bg-black/30 flex flex-col">
+  {token && (
+    <div className= "max-w-40 min-h-[20rem] mx-auto rounded-full object-fill flex flex-col justify-evenly">
+      <img className= "rounded-full object-cover" src="./src/assets/profile.jpg"></img>
+      <h3 className= "overflow-hidden font-semibold">{username}</h3>
+      <button className="border p-2 rounded w-full mb-2 hover:bg-white/10 active:bg-orange-700" onClick={() => {localStorage.removeItem("token");setToken("");setJobs([])}}>Logout</button>
+    </div>
+  )}
+  {!token && (
+    <div className="w-full min-h-[20rem] flex flex-col justify-center">
+      <h2 className="text-2xl font-bold mb-4">Login</h2>
 
-  <input placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-  <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-  
-  <button onClick={() => {login();fetchJobs()}}>Log In</button>
-  <h1>Job Tracker</h1>
-    <button onClick={fetchJobs}>Load Jobs</button>
+      <input className="bg-white/5 p-2 w-full text-center focus:bg-white/10 focus:outline-none" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input className="bg-white/5 p-2 w-full text-center focus:bg-white/10 focus:outline-none" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      
+      <button className="bg-white/5 p-2 mt-2 w-full hover:bg-white/10" onClick={() => {login();fetchJobs()}}>Log In</button>
+    </div>
+  )}
+    <div className=" border-white border-b-2 my-4 mx-20"></div>
+    <div className="flex flex-col">
+      <span className=" p-2 w-full text-center hover:bg-white/10 cursor-pointer active:bg-orange-700">Home</span>
+      <span className=" p-2 w-full text-center hover:bg-white/10 cursor-pointer active:bg-orange-700">Job Tracker</span>
+      <span className=" p-2 w-full text-center hover:bg-white/10 cursor-pointer active:bg-orange-700">Project 2</span>
+      <span className=" p-2 w-full text-center hover:bg-white/10 cursor-pointer active:bg-orange-700">Project 3</span>
+      <span className=" p-2 w-full text-center hover:bg-white/10 cursor-pointer active:bg-orange-700">Project 4</span>
+      <span className=" p-2 w-full text-center hover:bg-white/10 cursor-pointer active:bg-orange-700">Project 5</span>
+    </div>
+  </div>
 
-    <h2>Add Job</h2>
-      <input type="text" placeholder = "Job Title" value = {title} onChange = {(e) => setTitle(e.target.value)}/>
-      <input type="text" placeholder = "Company Name" value = {company} onChange = {(e) => setCompany(e.target.value)}/>
+  <div className="w-5/6 max-auto rounded-2xl shadow p-6">
+    
+    <h1>Job Tracker</h1>
+      <button onClick={fetchJobs}>Load Jobs</button>
 
-      <select name="status" id="status" value = {status} onChange = {(e) => setStatus(e.target.value)}>
-        <option value="applied">Applied</option>
-        <option value="rejected">Rejected</option>
-        <option value="interview">Interview</option>
-      </select>
+      <h2 className="text-xl font-semibold mt-6 mb-2">Add Job</h2>
+        <input className="border p-2 rounded w-full mb-2" type="text" placeholder = "Job Title" value = {title} onChange = {(e) => setTitle(e.target.value)}/>
+        <input className="border p-2 rounded w-full mb-2" type="text" placeholder = "Company Name" value = {company} onChange = {(e) => setCompany(e.target.value)}/>
 
-      <button onClick={addOrUpdateJob}>Create</button>
+        <select className="border p-2 rounded w-full mb-2" name="status" id="status" value = {status} onChange = {(e) => setStatus(e.target.value)}>
+          <option value="applied">Applied</option>
+          <option value="rejected">Rejected</option>
+          <option value="interview">Interview</option>
+        </select>
 
-    <h2>Search</h2>
-      <input placeholder="Search by: Title, Company" value={search} onChange={(e) => setSearch(e.target.value)}/>
-      <button onClick={fetchJobs}>Search</button>
-      <select value = {searchstatus} onChange = {(e) => setSearchStatus(e.target.value)}>
-        <option value="" default>All</option>
-        <option value="applied">Applied</option>
-        <option value="rejected">Rejected</option>
-        <option value="interview">Interview</option>
-      </select>
-      <div>
-        <button onClick={() => setPage(prev => Math.max(prev-1, 1))} disabled={page === 1}>Prev</button>
-        <span>Page {page} of {Math.ceil(total / limit)}</span>
-        <button onClick={() => setPage(prev => Math.min(prev+1, Math.ceil(total / limit)))} disabled={page === Math.ceil(total / limit)}>Next</button>
-      </div>
-      <ul>
-        {jobs.map((job) => (
-          <li key = {job.id}>
-            {job.title} - {job.company} - {job.status} 
+        <button className="border p-2 rounded w-full mb-2" onClick={addOrUpdateJob}>Create</button>
 
-            <button onClick={() => startEdit(job)}>Edit</button>
+      <h2 className="text-xl font-semibold mt-4">Search</h2>
+        <input className="border p-2 rounded w-full mb-2 mt-2" placeholder="Search by: Title, Company" value={search} onChange={(e) => setSearch(e.target.value)}/>
+        <button className="border p-2 rounded w-full mb-2" onClick={fetchJobs}>Search</button>
+        <select value = {searchstatus} onChange = {(e) => setSearchStatus(e.target.value)}>
+          <option value="" default>All</option>
+          <option value="applied">Applied</option>
+          <option value="rejected">Rejected</option>
+          <option value="interview">Interview</option>
+        </select>
+        <div className="flex justify-between items-center mt-4 mb-2">
+          <button className="bg-gray-700 px-3 py-1 rounded" onClick={() => setPage(prev => Math.max(prev-1, 1))} disabled={page === 1}>Prev</button>
+          <span className="text-sm">Page {page} of {Math.ceil(total / limit)}</span>
+          <button className="bg-gray-700 px-3 py-1 rounded" onClick={() => setPage(prev => Math.min(prev+1, Math.ceil(total / limit)))} disabled={page === Math.ceil(total / limit)}>Next</button>
+        </div>
+        <ul className="space-y-3">
+          {jobs.map((job) => (
+            
+            <li key = {job.id} className="border rounded p-4 shadow-sm">
 
-            {editingId && (<button onClick={() => { setEditingId(null), setTitle(""), setCompany(""), setStatus("")}}>Cancel</button>)}
+              <div className="flex justify-between items-center">
+                <div>
+                  <p className="flex justify-between items-center">{job.title}</p>
+                  <p className="text-sm ">{job.company}</p>
+                  <p className="text-xs mt-1">{job.status}</p>  
+                </div>
+                <div className="space-x-2">
+                  <button className="bg-gray-700 px-2 py-1 rounded" onClick={() => startEdit(job)}>Edit</button>
+                  {editingId && (<button onClick={() => { setEditingId(null), setTitle(""), setCompany(""), setStatus("")}}>Cancel</button>)}
+                </div>
+                <div className="bg-red-700 px-2 py-1 rounded">
+                  <button onClick={() => deleteJob(job.id)}>Delete</button>
+                </div>
+              </div>
 
-            <button onClick={() => deleteJob(job.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-
-
-
-
+            </li>
+          ))}
+        </ul>
+  </div>
 </div>
   )
 }
