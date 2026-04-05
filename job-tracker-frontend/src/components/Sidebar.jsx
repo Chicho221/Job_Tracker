cb22125561f8059e1205e2311773fdcfdb0598d6
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-04-05 19:53:52 +0200

    Add DELETE account button

--- job-tracker-frontend/src/components/Sidebar.jsx
+++ job-tracker-frontend/src/components/Sidebar.jsx
@@ -21,7 +21,7 @@ function SideBar({loading, password, login, setPassword,setNewpassword, newpassw
                     <input className="bg-white/5 p-2 w-full text-center focus:bg-white/10 focus:outline-none" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
                     <input className="bg-white/5 p-2 w-full text-center focus:bg-white/10 focus:outline-none" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                     
-                    <button className="transition duration-300 ease-in-out p-2 rounded w-full mb-2 shadow-lg shadow-blue-700/50 bg-blue-700 hover:bg-white hover:text-blue-700 flex justify-center disabled:bg-zinc-700 disabled:shadow-l disabled:shadow-zinc-700 active:scale-95" onClick={() => {login(),setPassword("")}}disabled = {loading}>
+                    <button className="transition duration-100 ease-in-out p-2 rounded w-full mb-2 shadow-lg shadow-blue-700/50 bg-blue-700 hover:bg-white hover:text-blue-700 flex justify-center disabled:bg-zinc-700 disabled:shadow-l disabled:shadow-zinc-700 active:scale-95" onClick={() => {login(),setPassword("")}}disabled = {loading}>
                         {!loading &&("Login")}
                         {loading &&(<div className="rounded-full w-6 h-6 border-2 border-white border-t-transparent animate-spin"></div>)}
                     </button>
@@ -29,10 +29,10 @@ function SideBar({loading, password, login, setPassword,setNewpassword, newpassw
                     <h2 className="text-2xl font-bold mb-4 font-mono">Register</h2>
                     <input className="bg-white/5 p-2 w-full text-center focus:bg-white/10 focus:outline-none" placeholder="Username" value={newusername} onChange={(e) => setNewusername(e.target.value)} />
                     <input className="bg-white/5 p-2 w-full text-center focus:bg-white/10 focus:outline-none" placeholder="Password" value={newpassword} onChange={(e) => setNewpassword(e.target.value)} />
-                    <button className="transition duration-300 ease-in-out p-2 rounded w-full mb-2 shadow-lg shadow-blue-700/50 bg-blue-700 hover:bg-white hover:text-blue-700 flex justify-center disabled:bg-zinc-700 disabled:shadow-l disabled:shadow-zinc-700 active:scale-95" onClick={createUser} disabled = {loading}>
+                    <button className="transition duration-100 ease-in-out p-2 rounded w-full mb-2 shadow-lg shadow-blue-700/50 bg-blue-700 hover:bg-white hover:text-blue-700 flex justify-center disabled:bg-zinc-700 disabled:shadow-l disabled:shadow-zinc-700 active:scale-95" onClick={createUser} disabled = {loading}>
                         {!loading &&("Register")}
                         {loading &&(<div className=" rounded-full w-6 h-6 border-2 border-white border-t-transparent animate-spin"></div>)}
-                        </button>
+                    </button>
                 </div>
             </div>
             
@@ -44,7 +44,7 @@ function SideBar({loading, password, login, setPassword,setNewpassword, newpassw
             <div className= "max-w-40 min-h-[20rem] mx-auto rounded-full object-fill flex flex-col justify-evenly">
                 <img className= "animate-pulse-shadow shadow-blue-700 rounded-full object-cover " src={profile}></img>
                 <h3 className= "overflow-hidden font-semibold cursor-default">{username}</h3>
-                <button className="transition duration-300 ease-in-out p-2 rounded w-full mb-2 shadow-lg shadow-blue-700/50 bg-blue-700 hover:bg-white hover:text-blue-700 flex justify-center disabled:bg-zinc-700 disabled:shadow-l disabled:shadow-zinc-700 active:scale-95" onClick={logout} disabled = {loading}>
+                <button className="transition duration-100 ease-in-out p-2 rounded w-full mb-2 shadow-lg shadow-blue-700/50 bg-blue-700 hover:bg-white hover:text-blue-700 flex justify-center disabled:bg-zinc-700 disabled:shadow-l disabled:shadow-zinc-700 active:scale-95" onClick={logout} disabled = {loading}>
                 {!loading &&("Logout")}
                 {loading &&(<div className=" rounded-full w-6 h-6 border-2 border-white border-t-transparent animate-spin"></div>)}
                 </button>
@@ -60,6 +60,9 @@ function SideBar({loading, password, login, setPassword,setNewpassword, newpassw
                 <span className="transition duration-300 ease-in-out p-2 w-full text-center hover:bg-blue-700 cursor-pointer active:bg-blue-700">Project 4</span>
                 <span className="transition duration-300 ease-in-out p-2 w-full text-center hover:bg-blue-700 cursor-pointer active:bg-blue-700">Project 5</span>
             </div>
+            <button className="flex flex-col rounded-full p-2 mt-auto mb-4 mx-4 bg-red-800
+            transition duration-100 active:scale-95 hover:bg-white hover:text-red-800
+            disabled:bg-zinc-700" disabled = {loading}>Delete Account</button>
         </div>
     )}
 
